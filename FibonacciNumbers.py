9fac0009d35513c06229a7e5e188c90fabc26291
jackleekopij <jack.lee-kopij@woodside.com.au>
2017-06-11 01:57:19 -0700

    Create to recursive programming technique

--- FibonacciNumbers.py
+++ FibonacciNumbers.py
@@ -1,21 +1,8 @@
 #### Script to print out Fibonacci numbers.
 
 def fibonacciValues(n):
-  fibonacci_value = 2
-  immediate_previous = 1
-  second_previous = 1
-  if n  == 0 or n == 1:
-    return 1
-  elif n == 2:
-    return 2
-  else:
-    for i in range(2,n):
-      fibonacci_value = immediate_previous + second_previous
-      print("The immediate_previous was: " + str(immediate_previous))
-      print("The second previous was: " + str(second_previous))
-      print("The fibonaccie value was: " + str(fibonacci_value))
-      second_previous = immediate_previous
-      immediate_previous = fibonacci_value
-  return fibonacci_value
+  if n  == 0: return 0
+  elif n == 1: return 1
+  else: return fibonacciValues(n-1) + fibonacciValues(n-2)
 
 print(fibonacciValues(8))
